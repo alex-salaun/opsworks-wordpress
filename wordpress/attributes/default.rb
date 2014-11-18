f718caa2c57b0420c097bdb3593849338c75146d
Alexandre Salaun <asalaun@siliconsalad.com>
2014-11-18 18:31:51 +0100

    Debug attributes

--- wordpress/attributes/default.rb
+++ wordpress/attributes/default.rb
@@ -1,11 +1,18 @@
 # Default configuration for the AWS OpsWorks cookbook for Wordpress
 #
 
+Chef::Log.info("Set wordpress attributes")
+
 layer               = node[:opsworks][:instance][:layers].first
+Chef::Log.info("Layer : #{layer}")
 instance            = node[:opsworks][:layers].fetch(layer)[:instances].first
+Chef::Log.info("Instance : #{instance}")
 instance_elastic_ip = instance[:elastic_ip]
+Chef::Log.info("Instance_elastic_ip : #{instance_elastic_ip}")
 application_name    = node[:opsworks][:applications][0][:name]
+Chef::Log.info("application_name : #{application_name}")
 password            = deploy[application_name.to_sym][:environment][:ftp_user_password]
+Chef::Log.info("password : #{password}")
 
 default['wordpress']['wp_config']['ftp_host'] = instance_elastic_ip
 default['wordpress']['wp_config']['ftp_user'] = "ftpuser"
