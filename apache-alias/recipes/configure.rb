0f30cb320facbda24347c2835609b5a803fd4d81
Alexandre Salaun <asalaun@siliconsalad.com>
2014-11-19 21:45:57 +0100

    Update recipes

--- apache-alias/recipes/configure.rb
+++ apache-alias/recipes/configure.rb
@@ -1,30 +1,18 @@
 Chef::Log.info("Configure apache alias")
 
 node[:deploy].each do |app_name, deploy|
-    Chef::Log.info("app_name : #{app_name.gsub('-', '_')}")
 
-    template "/etc/apache2/sites-available/#{app_name.gsub('-', '_')}" do
-        source "site.conf.erb"
-        mode 0660
-        group deploy[:group]
+  if defined?(deploy[:application_type]) && deploy[:application_type] == 'php'
+    template "/etc/apache2/sites-available/#{app_name.gsub('-', '_')}.conf" do
+      source "site.erb"
 
-        if platform?("ubuntu")
-          owner "www-data"
-        elsif platform?("amazon")
-          owner "apache"
-        end
-
-        Chef::Log.info("app name : #{app_name}")
-        Chef::Log.info("app name : #{app_name.gsub('-', '_')}")
-        Chef::Log.info("deploy : #{deploy}")
-
-        variables(
-            :app_name   => app_name.gsub('-', '_'),
-            :domain     => (deploy[:domains].first),
-            :ftp_user   => deploy[:environment][:ftp_user_name],
-            :ftp_pass   => deploy[:environment][:ftp_user_password],
-            :site_url   => deploy[:environment][:blog_siteurl]
-        )
+      variables(
+        :app_name   => app_name.gsub('-', '_'),
+        :domain     => (deploy[:domains].first),
+        :ftp_user   => deploy[:environment][:ftp_user_name],
+        :ftp_pass   => deploy[:environment][:ftp_user_password],
+        :site_url   => deploy[:environment][:blog_siteurl]
+      )
     end
 
     service "apache2" do
@@ -34,4 +22,5 @@ node[:deploy].each do |app_name, deploy|
     service "apache2" do
       action :restart
     end
+  end
 end
--- nginx-proxypass/recipes/configure.rb
+++ nginx-proxypass/recipes/configure.rb
@@ -3,33 +3,23 @@ Chef::Log.info("Configure Nginx Proxy Pass")
 node[:deploy].each do |app_name, deploy|
   Chef::Log.info("app_name : #{app_name.gsub('-', '_')}")
 
-  template "/etc/nginx/sites-available/#{app_name.gsub('-', '_')}" do
+  if defined?(deploy[:application_type]) && deploy[:application_type] == 'rails'
+    template "/etc/nginx/sites-available/#{app_name.gsub('-', '_')}" do
       source "site.erb"
-      mode 0660
-      group deploy[:group]
-
-      if platform?("ubuntu")
-        owner "www-data"
-      elsif platform?("amazon")
-        owner "apache"
-      end
-
-      Chef::Log.info("app name : #{app_name}")
-      Chef::Log.info("app name : #{app_name.gsub('-', '_')}")
-      Chef::Log.info("deploy : #{deploy}")
 
       variables(
-          :proxy_url  => deploy[:environment][:proxy_url],
-          :app_name   => app_name.gsub('-', '_'),
-          :domain     => (deploy[:domains].first)
+        :proxy_url  => deploy[:environment][:proxy_url],
+        :app_name   => app_name.gsub('-', '_'),
+        :domain     => (deploy[:domains].first)
       )
-  end
+    end
 
-  service "nginx" do
-    action :reload
-  end
+    service "nginx" do
+      action :reload
+    end
 
-  service "nginx" do
-    action :restart
+    service "nginx" do
+      action :restart
+    end
   end
 end
