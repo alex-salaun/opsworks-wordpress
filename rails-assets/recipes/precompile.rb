01b6f041f068f32801e2da8252266f904fcab32b
Alexandre Salaun <asalaun@siliconsalad.com>
2014-11-21 14:06:03 +0100

    Update precompile task

--- rails-assets/recipes/precompile.rb
+++ rails-assets/recipes/precompile.rb
@@ -3,12 +3,12 @@ node[:deploy].each do |application, deploy|
     rails_env    = deploy[:rails_env]
     current_path = deploy[:current_path]
 
-    Chef::Log.info("Precompiling Rails assets with environment #{rails_env}")
+    Chef::Log.info("Precompiling Rails assets with environment #{rails_env} and restart")
 
     execute 'rake assets:precompile' do
       cwd current_path
       user 'deploy'
-      command 'bundle exec rake assets:precompile'
+      command "bundle exec rake assets:precompile && #{node[:opsworks][:rails_stack][:restart_command]}"
       environment 'RAILS_ENV' => rails_env
     end
   end
