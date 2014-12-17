ece73749cef8ce8767141a69331ed6ad8f2806f9
Alexandre Salaun <asalaun@siliconsalad.com>
2014-12-17 15:29:20 +0100

    Update nginx recipe for htaccess

--- nginx-custom/recipes/configure.rb
+++ nginx-custom/recipes/configure.rb
@@ -4,6 +4,9 @@ node[:deploy].each do |app_name, deploy|
   if defined?(deploy[:application_type]) && deploy[:application_type] == 'rails'
     Chef::Log.info("Update conf file : /etc/nginx/sites-available/#{app_name.gsub('-', '_')}")
 
+    htaccess_username = node[:custom_nginx][:htaccess_username]
+    htaccess_password = node[:custom_nginx][:htaccess_password]
+
     template "/etc/nginx/sites-available/#{app_name.gsub('-', '_')}" do
       source "site.erb"
 
@@ -12,6 +15,16 @@ node[:deploy].each do |app_name, deploy|
         :app_name   => app_name.gsub('-', '_'),
         :domain     => (deploy[:domains].first)
       )
+    end
+
+    script "add_htpasswd" do
+      interpreter "bash"
+      user "root"
+      cwd "/"
+      code <<-EOH
+      touch /etc/nginx/conf.d/htpasswd
+      printf "#{htaccess_username}:$(openssl passwd -crypt #{htaccess_password})\n\" | tee /etc/nginx/conf.d/htpasswd
+      EOH
     end
 
     service "nginx" do
