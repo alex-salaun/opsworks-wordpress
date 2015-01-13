c022ae2fca829f4f57941e25428266e27fb8bf77
Alexandre Salaun <asalaun@siliconsalad.com>
2015-01-13 15:05:25 +0100

    Update ghost recipe

--- ghost/recipes/deploy.rb
+++ ghost/recipes/deploy.rb
@@ -1,6 +1,11 @@
 node[:deploy].each do |app_name, deploy|
   Chef::Log.info("Start ghost server")
 
+  if defined?(deploy[:application_type]) && deploy[:application_type] != 'static'
+    Chef::Log.debug("Skipping ghost configure application #{app_name} as it is not defined as ghost blog")
+    next
+  end
+
   script "start_npm" do
     interpreter "bash"
     user "deploy"
