6ce45e226ac88f3b96ad87958b52440f54159bae
Alexandre Salaun <asalaun@siliconsalad.com>
2015-01-14 09:28:47 +0100

    Update ghost recipe

--- ghost/recipes/configure.rb
+++ ghost/recipes/configure.rb
@@ -1,36 +1,33 @@
 node[:deploy].each do |app_name, deploy|
-  Chef::Log.info("Setup Ghost")
+  if defined?(deploy[:application_type]) && deploy[:application_type] == 'static'
+    Chef::Log.info("Setup Ghost for #{app_name}")
 
-  if defined?(deploy[:application_type]) && deploy[:application_type] != 'static'
-    Chef::Log.debug("Skipping ghost configure application #{app_name} as it is not defined as ghost blog")
-    next
-  end
+    template "#{deploy[:deploy_to]}/current/config.js" do
+      source "config.js.erb"
+      group deploy[:group]
 
-  template "#{deploy[:deploy_to]}/current/config.js" do
-    source "config.js.erb"
-    group deploy[:group]
+      if platform?("ubuntu")
+        owner "www-data"
+      elsif platform?("amazon")
+        owner "apache"
+      end
 
-    if platform?("ubuntu")
-      owner "www-data"
-    elsif platform?("amazon")
-      owner "apache"
-    end
-
-    variables(
+      variables(
         :database   => (deploy[:database][:database] rescue nil),
         :user       => (deploy[:database][:username] rescue nil),
         :password   => (deploy[:database][:password] rescue nil),
         :host       => (deploy[:database][:host] rescue nil),
         :domain     => (deploy[:domains].first)
-    )
-  end
+      )
+    end
 
-  script "setup_ghost" do
-    interpreter "bash"
-    user "deploy"
-    cwd "#{deploy[:deploy_to]}/current/"
-    code <<-EOH
+    script "setup_ghost" do
+      interpreter "bash"
+      user "deploy"
+      cwd "#{deploy[:deploy_to]}/current/"
+      code <<-EOH
         npm install --production
-    EOH
+      EOH
+    end
   end
 end
--- ghost/recipes/deploy.rb
+++ ghost/recipes/deploy.rb
@@ -1,17 +1,14 @@
 node[:deploy].each do |app_name, deploy|
-  Chef::Log.info("Start ghost server")
+  if defined?(deploy[:application_type]) && deploy[:application_type] == 'static'
+    Chef::Log.info("Start ghost server #{app_name}")
 
-  if defined?(deploy[:application_type]) && deploy[:application_type] != 'static'
-    Chef::Log.debug("Skipping ghost configure application #{app_name} as it is not defined as ghost blog")
-    next
-  end
-
-  script "start_npm" do
-    interpreter "bash"
-    user "deploy"
-    cwd "#{deploy[:deploy_to]}/current/"
-    code <<-EOH
-        npm start --production
-    EOH
+    script "start_npm" do
+      interpreter "bash"
+      user "deploy"
+      cwd "#{deploy[:deploy_to]}/current/"
+      code <<-EOH
+          npm start --production
+      EOH
+    end
   end
 end
