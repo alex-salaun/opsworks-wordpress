2cb3422019b1d411bbc5319e050ef62579bfd6cc
Alexandre Salaun <asalaun@siliconsalad.com>
2015-01-15 16:47:46 +0100

    Add debugger

--- nginx/attributes/default.rb
+++ nginx/attributes/default.rb
@@ -3,6 +3,9 @@ Chef::Log.info("Set nginx attributes")
 my_ip = '10.0.0.0/8'
 
 application_name = node[:opsworks][:applications].each do |application|
+  Chef::Log.info("Application : #{application.inspect}")
+  Chef::Log.info("Deploy : #{deploy.inspect}")
+
   application_name = application[:name].gsub('-', '_')
   unless deploy[application_name.to_sym][:environment][:my_ip].blank?
     my_ip = deploy[application_name.to_sym][:environment][:my_ip]
