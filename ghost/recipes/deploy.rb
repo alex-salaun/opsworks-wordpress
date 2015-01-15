5dacc36b9ed41e733d39309d3184777900ca4c7a
Alexandre Salaun <asalaun@siliconsalad.com>
2015-01-15 11:50:56 +0100

    Update ghost recipe

--- ghost/recipes/deploy.rb
+++ ghost/recipes/deploy.rb
@@ -2,6 +2,25 @@ node[:deploy].each do |app_name, deploy|
   if defined?(deploy[:application_type]) && deploy[:application_type] == 'static'
     Chef::Log.info("Start ghost server #{app_name}")
 
+    template "#{deploy[:deploy_to]}/current/config.js" do
+      source "config.js.erb"
+      group deploy[:group]
+
+      if platform?("ubuntu")
+        owner "www-data"
+      elsif platform?("amazon")
+        owner "apache"
+      end
+
+      variables(
+        :database   => (deploy[:database][:database] rescue nil),
+        :user       => (deploy[:database][:username] rescue nil),
+        :password   => (deploy[:database][:password] rescue nil),
+        :host       => (deploy[:database][:host] rescue nil),
+        :domain     => (deploy[:domains].first)
+      )
+    end
+
     script "start_npm" do
       interpreter "bash"
       user "root"
